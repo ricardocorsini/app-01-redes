ac4a6f762f2e714441ce751b5ddc429ab012a0b6
ricardocorsini <corsini.eng@gmail.com>
2024-05-07 20:56:05 -0300

    insercao de nova rota para estacaZero

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, send_file
 from waitress import serve
 from DimCis import Beam
+from EstacaZero import wordExport
 
 app = Flask(__name__, template_folder='template')
 
@@ -36,7 +37,36 @@ def submit():
         print(f"Recebido: {name}, {bw}, {d}, {vk}, {gama_c}, {gama_c2}, {fywk}, {gama_s}, {fck}, {stittupleg}")
 
         return send_file(path_memory, as_attachment=True, download_name=path_memory)
+    
+@app.route('/submitEstaca', methods=['POST', 'GET'])
+def submitEstaca():
 
+    if request.method == 'POST':
+
+        listaSolos = str(request.form.get('listaSolos'))
+        listaSolos_str = listaSolos.split(",")
+        listaSolos_int = [int(num.strip()) for num in listaSolos_str]
+
+
+        listaNspt = str(request.form.get('listaNspt'))
+        listaNspt_str = listaNspt.split(",")
+        listaNspt_int = [int(num.strip()) for num in listaNspt_str]
+
+
+        tipoEstaca = str(request.form.get('tipoEstaca'))
+        diametroEstaca = float(request.form.get('diametroEstaca'))
+        cargaAdmissivel = float(request.form.get('cargaAdmissivel'))
+        nivelAgua = float(request.form.get('nivelAgua'))
+        fileName = str(request.form.get('fileName'))
+                      
+       
+        wordExport(listaSolos_int, tipoEstaca, diametroEstaca, listaNspt_int, cargaAdmissivel, nivelAgua, fileName)
+        print(f"Recebido: {listaSolos_int}, {listaNspt_int}, {tipoEstaca}, {diametroEstaca}, {cargaAdmissivel}")
+
+        path_memory = fileName + '.docx'
+
+        return send_file(path_memory, as_attachment=True, download_name=path_memory)
+        
 
 if __name__ == '__main__':
     #local use
